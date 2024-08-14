96d22f88229a080242763748f4872d6b408e72fa
samthecodeking <sam.leigh1987@gmail.com>
2024-08-14 13:18:18 +0100

    Fixed code + wrote book report

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-character_dict = {}
-lower_case = text.lower()
-sorted_dict = {}
-
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_word_count(text)
-    num_char = get_char_count(text)
-    print(f"{num_words} words found in the document")
-    print(num_char)
+    char_dict = get_char_count(text)
+    char_dict_sorted = char_dict_to_sorted_list(char_dict)
 
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    
+    for i in char_dict_sorted:
+        print(f"The '{i['Char']}' character was found {i['num']} times")
+    
+    print("--- End report ---")
 
 def get_book_text(path):
     with open(path) as f:
@@ -17,31 +19,35 @@ def get_book_text(path):
     
 def get_word_count(text):
     words = text.split()
-    word_count = 0
-
-    for word in words:
-        word_count += 1
-    return word_count
+    return len(words)
 
 def get_char_count(text):
-
+    character_dict = {}
+    lower_case = text.lower()
+    # sorted_dict = {}
     for char in lower_case:
-        if char in character_dict:
-            character_dict[char] += 1
-        else:
-            character_dict[char] = 1
+        if char.isalpha(): #checks if char is alphabetic
+            if char in character_dict:
+                character_dict[char] += 1
+            else:
+                character_dict[char] = 1
     
-    for key in sorted(character_dict.keys()):
-        sorted_dict[key] = character_dict[key]
+    return character_dict
+
+    # for key in sorted(character_dict.keys()):
+    #     sorted_dict[key] = character_dict[key]
             
-    return sorted_dict
+    # return sorted_dict
 
-def sort_on(sorted_dict):
-    return sorted_dict["num"]
+def sort_on(dict):
+    return dict["num"]
 
-def get_book_report(text):
-    sorted_list = list(sorted_dict())
+def char_dict_to_sorted_list(num_char_dict):
+    sorted_list = []
+    for i in num_char_dict:
+        sorted_list.append({"Char": i , "num": num_char_dict[i]})
     sorted_list.sort(reverse=True, key=sort_on)
 
+    return (sorted_list)
 
 main()
